a3051f01752aeb4f47de3dcbb791687f33c1415d
Daniel Dauber <daniel.dauber@sap.com>
2022-03-29 11:36:49 -0300

    Completed module

--- vehicle.go
+++ vehicle.go
@@ -113,15 +113,13 @@ func generateRating() {
 
 		for _, msg := range v.Feedback {
 
-			text := strings.Split(msg, " ")
-
-			if len(text) >= 5 {
+			if text := strings.Split(msg, " "); len(text) >= 5 {
 				vehRating = 5.0
 				vehResult.feedbackTotal++
 
 				for _, word := range text {
 
-					switch word {
+					switch s := strings.Trim(strings.ToLower(word), " ,.,!,?,\t,\n,\r"); s {
 
 					case "pleasure", "impressed", "wonderful", "fantastic", "splendid":
 						vehRating += extraPositive
